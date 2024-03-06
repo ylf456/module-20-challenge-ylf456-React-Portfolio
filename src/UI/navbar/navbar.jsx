eea0e865a04461ae1af8a6219f4a413d255e901c
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-03-06 15:25:49 -0800

    added highlight feature when one link on the tab is selected

--- src/UI/navbar/navbar.jsx
+++ src/UI/navbar/navbar.jsx
@@ -2,24 +2,26 @@
 import { Link } from "react-router-dom"; // Link is method that takes the page to the route without fully reload the page
 import NavbarComponent from "./navbarComponent";
 import { useState } from "react";
+import "./index.css";
 
 export default function NavBar() {
-  const [select, setSelect] = useState("");
-
+  const [currentPage, setCurrentPage] = useState('/');
+ 
+  
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
     <NavbarComponent
       links={[
-        <Link key={1} className="nav-link text-light" to="/">
+        <Link key={1} onClick={()=> setCurrentPage('/')} className={currentPage === `/` ? 'nav-link text-light selected' : 'nav-link text-light'} to="/">
           About Me
         </Link>,
-        <Link key={2} className="nav-link text-light" to="/portfolio">
+        <Link key={2} onClick={()=> setCurrentPage('/portfolio')} className={currentPage === `/portfolio` ? 'nav-link text-light selected' : 'nav-link text-light'}  to="/portfolio">
           Profolio
         </Link>,
-        <Link key={3} className="nav-link text-light" to="/contact-me">
+        <Link key={3} onClick={()=> setCurrentPage('/contact-me')} className={currentPage === `/contact-me` ? 'nav-link text-light selected' : 'nav-link text-light'}  to="/contact-me">
           Contact Me
         </Link>,
-        <Link key={4} className="nav-link text-light" to="/resume">
+        <Link key={4} onClick={()=> setCurrentPage('/resume')} className={currentPage === `/resume` ? 'nav-link text-light selected' : 'nav-link text-light'}  to="/resume">
           Resume
         </Link>,
       ]}
--- src/pages/ContactMePage.jsx
+++ src/pages/ContactMePage.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 export default function ContactMePage() {
   return (
-    <div id="body-content">
+    <div className="body-content">
       <h1>ContactMe Page title</h1>
       <p>ContactMe page content</p>
     </div>
