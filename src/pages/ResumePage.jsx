d4ed225d5482bfc4768dd92d6851f14e15f2309b
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-03-07 15:50:21 -0800

    staging file

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,8 +1,16 @@
 export default function ResumePage() {
     return (
       <div className="body-content">
-        <h1>ResumePage title</h1>
+        <h1>Resume</h1>
         <p>ResumePage content</p>
+        <p></p>
+        <p></p>
+        <div className="download-container">
+        <a href='/assets/PDF/Yilun+Fan+CV+2023.pdf' download>
+        <h2>Download My Resume</h2>
+        </a>
+        <img src="/assets/images/download.png"></img>
+        </div>
       </div>
     );
   }
