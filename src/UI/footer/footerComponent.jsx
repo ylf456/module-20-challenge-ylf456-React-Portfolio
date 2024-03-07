d8c035355db0cdaf99ad8919ee1791938532ae9f
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-03-06 21:22:27 -0800

    complete about me page

--- src/UI/footer/footerComponent.jsx
+++ src/UI/footer/footerComponent.jsx
@@ -6,7 +6,7 @@ footerContainer:{
 
 export default function FooterComponent({ links }) {
     return (
-      <footer className="bg-dark text-light text-center py-2 text-lg">
+      <footer className="bg-dark text-light text-center py-0.5 text-lg">
         <div className="container">
           <div>
             <ul>
